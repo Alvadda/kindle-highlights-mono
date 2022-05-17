8b700d235f1ffea0b9f5d613c9cbb14159cb3964
Alvadda <58438582+Alvadda@users.noreply.github.com>
2022-05-17 08:09:23 +0200

    add apikey middleware

--- express/src/api.ts
+++ express/src/api.ts
@@ -2,12 +2,14 @@ import express from 'express'
 
 import highlightsRoute from './routes/highlights.routes'
 import booksRoute from './routes/books.routes'
+import apiKey from './middleware/apikey.middleware'
 
 const app = express()
 
 // Config
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(apiKey)
 
 // Routes
 app.use('/highlights', highlightsRoute)
---
+++ express/src/middleware/apikey.middleware.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express'
+import { prisma } from '../utils/db'
+
+const API_KEY_HEADER = 'api-key'
+
+export default async function (req: Request, res: Response, next: NextFunction) {
+  const apiKey = req.headers[API_KEY_HEADER]
+
+  if (typeof apiKey !== 'string') return res.status(401).send({ error: 'No api-key' })
+
+  const existKey = Boolean(await prisma.apikey.findUnique({ where: { id: apiKey } }))
+
+  if (!existKey) return res.status(401).send({ error: 'Invalid api-key' })
+
+  next()
+}
